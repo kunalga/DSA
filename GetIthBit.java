f564f8e010bf145bd88a9bfe7740aff0a15a2964
Kunal Garg <kunalgarg91189@gmail.com>
2025-02-13 00:23:21 +0530

    Bit Manupulation

--- GetIthBit.java
+++ GetIthBit.java
@@ -6,10 +6,32 @@ public class GetIthBit {
         }
         return 0;
     }
+    public static int clearBit(int num ,int inx) {
+    	int bitMask = 1<<inx;
+    	return (~(bitMask)&num);
+    	
+    }
+    public static int setIthBit(int num,int inx) {
+    	int bitMask = 1<<inx;
+    	
+    	return (bitMask | num );
+    }
+    public static int updateBit(int num ,int inx,int bit) {
+    	int bitClear = clearBit(num,inx);
+    	
+    	int bitMask = bit<<inx; 
+    	return bitMask | bitClear;
+    }
 
     public static void main(String[] args) {
-        // Get Ith Bit
-        int num = 7;  
+        int num = 5;  
         System.out.println(getIthBit(num, 0)); 
+        System.out.println(clearBit(num,0));
+        System.out.println(setIthBit(num,1));
+        System.out.println(updateBit(num ,4,1));
+        
+        
+        
+        
     }
 }

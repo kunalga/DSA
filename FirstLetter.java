639d239fbc50e490eca3b18770426c6f574df9f4
Kunal Garg <kunalgarg91189@gmail.com>
2025-02-12 22:59:01 +0530

    Day 5

--- FirstLetter.java
+++ FirstLetter.java
@@ -2,7 +2,23 @@
 public class FirstLetter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// aaabbcccd
+		String s1 ="aaabbccd";
+		StringBuilder sb = new StringBuilder("");
+		for(int i=0;i<s1.length();i++) {
+			int count =1;
+			while(i<s1.length()-1 && s1.charAt(i)==s1.charAt(i+1)) {
+				count++;
+				i++;
+				
+			}
+			sb.append(s1.charAt(i));
+			if(count>1) {
+				sb.append(count);
+				
+			}
+		}
+		System.out.println(sb);
 
 	}
 
--- GetIthBit.java
+++ GetIthBit.java
@@ -1,9 +1,15 @@
-
 public class GetIthBit {
+    public static int getIthBit(int num, int inx) {
+        int bitMask = 1 << inx;
+        if ((bitMask & num) != 0) { 
+            return 1;
+        }
+        return 0;
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        // Get Ith Bit
+        int num = 7;  
+        System.out.println(getIthBit(num, 0)); 
+    }
 }

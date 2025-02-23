b4c76c2f79f380247c8733797322d8857cd222a1
Kunal Garg <kunalgarg91189@gmail.com>
2025-02-23 22:16:03 +0530

    day -9

---
+++ QuckSort.java
@@ -0,0 +1,46 @@
+public class QuckSort {
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (i < high && arr[i] <= pivot) {
+                i++;
+            }
+            while (j > low && arr[j] > pivot) {
+                j--;
+            }
+            if (i < j) {
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, low, j);
+        return j;
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 43, 25, 78, 29};
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
